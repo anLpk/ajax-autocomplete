65d1da56ee0bb68b7afe0c7cff384d33969a3d51
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-12-12 13:24:57 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: Autocomplete the input with AJAX calls.
+// GET https://wagon-dictionary.herokuapp.com/autocomplete/:stem
+// https://wagon-dictionary.herokuapp.com/autocomplete/u
+// https://wagon-dictionary.herokuapp.com/autocomplete/un
+// https://wagon-dictionary.herokuapp.com/autocomplete/und
+// https://wagon-dictionary.herokuapp.com/autocomplete/unde
+// https://wagon-dictionary.herokuapp.com/autocomplete/under
+const input = document.getElementById("search");
+const matchList = document.getElementById("results");
+
+const searchPlace = (event) => {
+  matchList.innerHTML = "";
+  const query = event.target.value;
+  fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.words &&
+        data.words.forEach((word) => {
+          // console.log(result);
+          const search = `<p>${word}</p>`;
+          results.insertAdjacentHTML("beforeend", search);
+        });
+    });
+};
+
+input.addEventListener("keyup", searchPlace);
